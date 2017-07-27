3752644bb261af91d16a9f98881513fa99e7df9d
Espen Halvorsen <espenh@unipluss.no>
2017-07-27 14:39:35 +0200

    7 Razor Views
    _ViewImports

--- core20/api/Startup.cs
+++ core20/api/Startup.cs
@@ -20,7 +20,8 @@ namespace api
             Configuration = builder.Build();
         }
 
-        public IConfiguration Configuration { get; }
+        //public IConfiguration Configuration { get; }
+        public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
